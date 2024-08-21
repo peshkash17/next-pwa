722c2a92b22fe1add0f5f81908bc87c18c4f4c73
kaleshubham899 <128207569+kaleshubham899@users.noreply.github.com>
2024-08-21 23:21:02 +0530

    pwa component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,10 @@
 'use client'
 import Image from "next/image";
 import { useState,useEffect } from "react";
-import PWAPrompt from 'react-ios-pwa-prompt'
+import PWADetector from "@/components/PwaDetector";
 export default function Home() {
 
-  const [showPrompt, setShowPrompt] = useState(false);
-
-  useEffect(() => {
-    // Check if the code is running in the browser
-    if (typeof window !== 'undefined') {
-      const isIos = () => {
-        const userAgent = window.navigator.userAgent.toLowerCase();
-        return /iphone|ipad|ipod/.test(userAgent);
-      };
-
-      // Check if the device is in standalone mode
-      const isInStandaloneMode = () => {
-        return 'standalone' in window.navigator && window.navigator.standalone;
-      };
-
-      // If it's an iOS device and not in standalone mode, show the prompt
-      if (isIos() && !isInStandaloneMode()) {
-        setShowPrompt(true);
-      }
-    }
-  }, []);
+ 
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -152,13 +132,7 @@ export default function Home() {
     
      
       </div>
-      {showPrompt && (
-        <PWAPrompt
-          timesToShow={3}
-          promptOnVisit={1}
-          copyTitle="Install My App"
-        />
-      )}
+      <PWADetector/>
     </main>
   );
 }
---
+++ components/PwaDetector.tsx
@@ -0,0 +1,41 @@
+'use client'
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import PWAPrompt from 'react-ios-pwa-prompt'
+
+export default function PWADetector() {
+  const [showPrompt, setShowPrompt] = useState(false);
+
+  useEffect(() => {
+    // Check if the code is running in the browser
+    if (typeof window !== 'undefined') {
+      const isIos = () => {
+        const userAgent = window.navigator.userAgent.toLowerCase();
+        return /iphone|ipad|ipod/.test(userAgent);
+      };
+
+      // Check if the device is in standalone mode
+      const isInStandaloneMode = () => {
+        return 'standalone' in window.navigator && window.navigator.standalone;
+      };
+
+      // If it's an iOS device and not in standalone mode, show the prompt
+      if (isIos() && !isInStandaloneMode()) {
+        setShowPrompt(true);
+      }
+    }
+  }, []);
+
+
+  return (
+    <div>
+      {showPrompt && (
+        <PWAPrompt
+          timesToShow={3}
+          promptOnVisit={1}
+          copyTitle="Install My App"
+        />
+      )}
+    </div>
+  );
+}
